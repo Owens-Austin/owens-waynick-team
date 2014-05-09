e23a9c2bdee252927fc90a2064baba2d3fe1374b
user <user@10.18.148.168>
2014-05-09 09:43:09 -0600

    initial git commit

---
+++ zeldaPokemon/src/byui/cit260/zeldaPokemon/model/Enemies.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.zeldaPokemon.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author user
+ */
+public class Enemies implements Serializable{
+    
+    private double stats;
+
+    public Enemies() {
+    }
+    
+    public double getStats() {
+        return stats;
+    }
+
+    public void setStats(double stats) {
+        this.stats = stats;
+    }
+    
+    
+    
+    
+    
+}
