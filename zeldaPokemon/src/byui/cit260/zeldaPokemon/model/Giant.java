849b24a1076fa78a22d8acef2f32697a08f78db5
byuigenetics <gene@GATSBY>
2014-05-13 09:59:56 -0600

    Giant enemy java class

---
+++ zeldaPokemon/src/byui/cit260/zeldaPokemon/model/Giant.java
@@ -0,0 +1,39 @@
+
+// Giant Base statistics to code later
+// HP:        12
+// Def:        1
+// Speed:      1
+// Attack:     3
+// EXP Reward: 7
+// Giant will be stronger monsters that will be slower in attack
+
+
+package byui.cit260.zeldaPokemon.model;
+
+/**
+ *
+ * @author user
+ */
+public class Giant extends Enemies{
+    
+    private double statistics;
+
+    public Giant() {
+    }
+
+    public double getStatistics() {
+        return statistics;
+    }
+
+    public void setStatistics(double statistics) {
+        this.statistics = statistics;
+    }
+    
+    
+    
+   
+    
+    
+    
+    
+}
